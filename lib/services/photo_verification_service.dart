cc6f39acf37cb105236e9ee535d332424271ffbc
Katende Chris Marvin <katendechris5@gmail.com>
2025-09-17 10:08:00 +0300

    latest

--- lib/services/photo_verification_service.dart
+++ lib/services/photo_verification_service.dart
@@ -2,7 +2,7 @@ import 'dart:convert';
 import 'package:http/http.dart' as http;
 
 class PhotoVerificationService {
-  static const String baseUrl = 'http://192.168.1.3:8000/api';
+  static const String baseUrl = 'http://192.168.1.19:8000/api';
   
   // Store authentication token
   static String? _authToken;
